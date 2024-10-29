296c381d8881459eac6416949301dcc36788a842
decobot <capy@deco.cx>
2024-10-29 17:38:31 -0300

    feat(database): loader now get product details page

--- loaders/product/details/productDetails.ts
+++ loaders/product/details/productDetails.ts
@@ -1,6 +1,10 @@
 import type { RequestURLParam } from "apps/website/functions/requestToParam.ts";
 import { AppContext } from "apps/records/mod.ts";
-import { Product } from "apps/commerce/types.ts";
+import {
+  ListItem,
+  ProductDetailsPage,
+  PropertyValue,
+} from "apps/commerce/types.ts";
 import {
   getProductBySku,
   getProductBySlug,
@@ -20,10 +24,57 @@ export default async function loader(
   { slug, useSkuAsSlug }: Props,
   req: Request,
   ctx: AppContext,
-): Promise<Product | null> {
+): Promise<ProductDetailsPage | null> {
   const url = new URL(req.url);
-  const result = useSkuAsSlug
+  const product = useSkuAsSlug
     ? await getProductBySku(slug, ctx, url)
     : await getProductBySlug(slug, ctx, url);
-  return result;
+
+  if (!product) {
+    return null;
+  }
+
+  const categories = product.additionalProperty?.filter(({ propertyID }) =>
+    propertyID === "CATEGORY"
+  );
+
+  const itemListElement = getBreadcrumbList(url, categories);
+
+  return {
+    "@type": "ProductDetailsPage",
+    product,
+    breadcrumbList: {
+      "@type": "BreadcrumbList",
+      itemListElement,
+      numberOfItems: itemListElement.length,
+    },
+    seo: {
+      title: product.name!,
+      description: product.description ?? "",
+      canonical: product.url ?? "",
+    },
+  };
+}
+
+function getBreadcrumbList(
+  url: URL,
+  categories?: PropertyValue[],
+): ListItem[] {
+  if (!categories) return [];
+
+  //Recursive
+  const buildUrl = (cat: PropertyValue): string => {
+    const parent = categories.find((c) => c.value === cat.subjectOf);
+    return parent ? `${buildUrl(parent)}/${cat.value}` : cat.value ?? "";
+  };
+
+  return categories.map((category) => {
+    const categoryUrl = buildUrl(category);
+    return {
+      "@type": "ListItem",
+      item: category.name ?? "",
+      url: new URL(categoryUrl, url.origin).href,
+      position: categoryUrl.split("/").length,
+    };
+  });
 }
