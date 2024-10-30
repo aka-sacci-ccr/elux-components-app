138031407b17e27c19d4d08ae8f61c1e98ad2060
oiviana <vianalucas80@gmail.com>
2024-10-30 19:59:06 -0300

    feat(product-page): first structure

---
+++ sections/Product/ProductDetails/ProductPage.tsx
@@ -0,0 +1,57 @@
+import { ProductDetailsPage } from "apps/commerce/types.ts";
+
+interface ProductPageProps {
+    /** @description product loader of the page */
+    page: ProductDetailsPage | null
+
+    breadCrumb: {
+        /** @description limit quantity of breadcrumb */
+        maxQuantity?: number
+        /** @description color of current item of breadcrumb */
+        selectedColor: string
+    }
+    infoBanners: {
+        backgroundColor: {
+            /** @description Primary background component */
+            primary: string;
+            /** @description Secondary background component */
+            secondary: string;
+        }
+        titleColor: {
+            /** @description Primary title color */
+            primary: string;
+            /** @description Secondary title color */
+            secondary: string;
+        }
+        contentColor: {
+            /** @description Primary text color */
+            primary: string;
+            /** @description Secondary text color */
+            secondary: string;
+        }
+    }
+    productMain: {
+        buyButton: {
+            /** @description Button background color */
+            background: string;
+            /** @description Button text color */
+            textColor: string;
+        }
+    }
+}
+
+export default function ProductPage({ page }: ProductPageProps) {
+
+    if (!page) return <></>
+
+
+    console.log("test :", page)
+
+
+    return (
+        <div>
+            Product page
+        </div>
+    );
+
+}
