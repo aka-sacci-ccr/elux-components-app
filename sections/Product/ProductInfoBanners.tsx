dac7847afea5f469f92d80b65ac16787ec6cc306
oiviana <vianalucas80@gmail.com>
2024-10-30 11:11:16 -0300

    feat:(info-banners): prop colors

--- sections/Product/ProductInfoBanners.tsx
+++ sections/Product/ProductInfoBanners.tsx
@@ -33,22 +33,28 @@ export default function ProductInfoBanners({ image, backgroundColor, titleColor,
         { firstText: "Lorem ipsum dolor", secondText: " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vel ultricies leo. Nam vel urna at mi ullamcorper sagittis. Aliquam erat volutpat." },
         { firstText: "Lorem ipsum dolor", secondText: " Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur vel ultricies leo. Nam vel urna at mi ullamcorper sagittis. Aliquam erat volutpat." },
     ];
-    // <div
-    //     key={index}
-    //     className={`flex flex-col ${index % 2 === 0 ? "lg:flex-row" : "lg:flex-row-reverse"
-    //         } items-center bg-gray-200 p-4 rounded-lg`}
-    // >
 
     return (
         <div className="w-full max-w-7xl lg:mx-auto">
             {test.map((item, index) => (
-                <div className={`flex flex-col ${index % 2 === 0 ? "lg:flex-row" : "lg:flex-row-reverse"}`}>
+                <div className={`flex flex-col ${index % 2 === 0 ? "lg:flex-row" : "lg:flex-row-reverse"}`}
+                    style={{
+                        backgroundColor: index % 2 === 0 ? backgroundColor.primary : backgroundColor.secondary
+                    }}>
                     <img src={image.src} alt={"teste"} />
                     <div class="w-full flex items-center bg-red-400 min-h-56 max-h-[516px]">
                         <div className="w-full max-w-[400px] h-full flex flex-col mx-auto text-left justify-center gap-4 p-3">
-                            <span className="text-xl">{item.firstText}</span>
-                            <p className="text-base font-light">
-                            {item.secondText}
+                            <span className="text-xl"
+                                style={{
+                                    color: index % 2 === 0 ? titleColor.primary : titleColor.secondary
+                                }}>
+                                {item.firstText}
+                            </span>
+                            <p className="text-base font-light"
+                                style={{
+                                    color: index % 2 === 0 ? contentColor.primary : contentColor.secondary
+                                }}>
+                                {item.secondText}
                             </p>
                         </div>
                     </div>
