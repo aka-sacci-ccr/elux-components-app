2e83c6095335ebe4632f37f8fd19ad9cce83b47c
decobot <capy@deco.cx>
2024-11-21 16:32:02 -0300

    feat(home): help section added

--- components/ui/CategoryCard.tsx
+++ components/ui/CategoryCard.tsx
@@ -9,7 +9,7 @@ export default function CategoryCard({ title, icon, url }: Props) {
       href={url ?? "#"}
       class="flex flex-col gap-2 h-20 w-full md:h-[116px] pt-1 md:pt-5 bg-white rounded-sm border border-base-200 items-center"
     >
-      <Icon id={icon} />
+      <Icon id={icon} height={24} width={24} />
       <span class="text-xs md:text-base text-center px-1 font-light">
         {title}
       </span>
---
+++ sections/Content/HelpCards.tsx
@@ -0,0 +1,56 @@
+import HelpCard from "../../components/ui/HelpCard.tsx";
+import { AvailableIcons } from "../../components/ui/Icon.tsx";
+import { AppContext } from "../../mod.ts";
+import { clx } from "../../utils/clx.ts";
+
+export interface Props {
+  title: string;
+  cards: HelpCard[];
+}
+
+export interface HelpCard {
+  /**
+   * @title Icon
+   * @format icon-select
+   * @options elux-components-app/loaders/availableIcons.ts
+   */
+  icon: AvailableIcons;
+  title: string;
+  description: string;
+  url?: string;
+}
+
+export function loader(
+  props: Props,
+  _req: Request,
+  { siteTemplate }: AppContext,
+) {
+  return { ...props, siteTemplate };
+}
+
+export default function HelpCards(
+  { title, cards, siteTemplate }: ReturnType<typeof loader>,
+) {
+  return (
+    <div class={"w-full bg-primary"}>
+      <div class="container pt-12 md:pt-20 pb-12 md:pb-24 flex flex-col gap-8 max-lg:px-5.5">
+        <h2
+          class={clx(
+            "font-medium text-2xl md:text-2.5xl md:font-semibold text-white",
+          )}
+        >
+          {title}
+        </h2>
+        <div
+          class={clx(
+            "grid grid-cols-1 md:grid-cols-3 gap-6 md:gap-5",
+          )}
+        >
+          {cards.map((card) => (
+            <HelpCard {...card} siteTemplate={siteTemplate} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
