ca7e6d44f3cf22729a7d1a4dcf755bd7f78d7088
decobot <capy@deco.cx>
2025-03-19 13:45:07 -0300

    raw text component added

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -40,41 +40,42 @@ import * as $$$11 from "./loaders/product/list/productList.ts";
 import * as $$$12 from "./loaders/product/listing/productListing.ts";
 import * as $$$13 from "./loaders/testShelf.ts";
 import * as $$$14 from "./loaders/whereToBuy.ts";
-import * as $$$$$$26 from "./sections/blog/BlogListing.tsx";
-import * as $$$$$$27 from "./sections/blog/BlogListingBreadcrumb.tsx";
-import * as $$$$$$30 from "./sections/blog/BlogpostBanner.tsx";
-import * as $$$$$$31 from "./sections/blog/BlogpostBreadcrumb.tsx";
-import * as $$$$$$32 from "./sections/blog/BlogpostComments.tsx";
-import * as $$$$$$33 from "./sections/blog/BlogpostContent.tsx";
-import * as $$$$$$28 from "./sections/blog/BlogSearch.tsx";
-import * as $$$$$$29 from "./sections/blog/BlogShelf.tsx";
+import * as $$$$$$27 from "./sections/blog/BlogListing.tsx";
+import * as $$$$$$28 from "./sections/blog/BlogListingBreadcrumb.tsx";
+import * as $$$$$$31 from "./sections/blog/BlogpostBanner.tsx";
+import * as $$$$$$32 from "./sections/blog/BlogpostBreadcrumb.tsx";
+import * as $$$$$$33 from "./sections/blog/BlogpostComments.tsx";
+import * as $$$$$$34 from "./sections/blog/BlogpostContent.tsx";
+import * as $$$$$$29 from "./sections/blog/BlogSearch.tsx";
+import * as $$$$$$30 from "./sections/blog/BlogShelf.tsx";
 import * as $$$$$$0 from "./sections/Component.tsx";
-import * as $$$$$$34 from "./sections/container/Container.tsx";
+import * as $$$$$$35 from "./sections/container/Container.tsx";
 import * as $$$$$$1 from "./sections/Content/Banner.tsx";
 import * as $$$$$$2 from "./sections/Content/Breadcrumb.tsx";
 import * as $$$$$$3 from "./sections/Content/Cards.tsx";
 import * as $$$$$$4 from "./sections/Content/CategoryCards.tsx";
 import * as $$$$$$5 from "./sections/Content/HelpCards.tsx";
 import * as $$$$$$6 from "./sections/Content/ImageCards.tsx";
-import * as $$$$$$7 from "./sections/Content/TextCards.tsx";
-import * as $$$$$$8 from "./sections/Content/Toast.tsx";
-import * as $$$$$$9 from "./sections/Footer/Footer.tsx";
-import * as $$$$$$10 from "./sections/Header/Header.tsx";
-import * as $$$$$$11 from "./sections/Images/BannersMatcher.tsx";
-import * as $$$$$$12 from "./sections/Images/Carousel.tsx";
-import * as $$$$$$13 from "./sections/Institutional/GuidesCategories.tsx";
-import * as $$$$$$14 from "./sections/Institutional/GuidesDocuments.tsx";
-import * as $$$$$$15 from "./sections/Institutional/GuidesProducts.tsx";
-import * as $$$$$$16 from "./sections/Institutional/Policy.tsx";
-import * as $$$$$$17 from "./sections/Institutional/Support.tsx";
-import * as $$$$$$18 from "./sections/Institutional/WhereToBuy.tsx";
-import * as $$$$$$19 from "./sections/PageView.tsx";
-import * as $$$$$$20 from "./sections/Product/ProductDetails/ProductBannerMatcher.tsx";
-import * as $$$$$$21 from "./sections/Product/ProductDetails/ProductBreadcrumb.tsx";
-import * as $$$$$$22 from "./sections/Product/ProductDetails/ProductPage.tsx";
-import * as $$$$$$23 from "./sections/Product/ProductListing/ProductListingPage.tsx";
-import * as $$$$$$24 from "./sections/Product/ProductShelf/ProductShelf.tsx";
-import * as $$$$$$25 from "./sections/Social/ContactForm.tsx";
+import * as $$$$$$7 from "./sections/Content/RawText.tsx";
+import * as $$$$$$8 from "./sections/Content/TextCards.tsx";
+import * as $$$$$$9 from "./sections/Content/Toast.tsx";
+import * as $$$$$$10 from "./sections/Footer/Footer.tsx";
+import * as $$$$$$11 from "./sections/Header/Header.tsx";
+import * as $$$$$$12 from "./sections/Images/BannersMatcher.tsx";
+import * as $$$$$$13 from "./sections/Images/Carousel.tsx";
+import * as $$$$$$14 from "./sections/Institutional/GuidesCategories.tsx";
+import * as $$$$$$15 from "./sections/Institutional/GuidesDocuments.tsx";
+import * as $$$$$$16 from "./sections/Institutional/GuidesProducts.tsx";
+import * as $$$$$$17 from "./sections/Institutional/Policy.tsx";
+import * as $$$$$$18 from "./sections/Institutional/Support.tsx";
+import * as $$$$$$19 from "./sections/Institutional/WhereToBuy.tsx";
+import * as $$$$$$20 from "./sections/PageView.tsx";
+import * as $$$$$$21 from "./sections/Product/ProductDetails/ProductBannerMatcher.tsx";
+import * as $$$$$$22 from "./sections/Product/ProductDetails/ProductBreadcrumb.tsx";
+import * as $$$$$$23 from "./sections/Product/ProductDetails/ProductPage.tsx";
+import * as $$$$$$24 from "./sections/Product/ProductListing/ProductListingPage.tsx";
+import * as $$$$$$25 from "./sections/Product/ProductShelf/ProductShelf.tsx";
+import * as $$$$$$26 from "./sections/Social/ContactForm.tsx";
 
 const manifest = {
   "loaders": {
@@ -95,46 +96,47 @@ const manifest = {
     "elux-components-app/loaders/whereToBuy.ts": $$$14,
   },
   "sections": {
-    "elux-components-app/sections/blog/BlogListing.tsx": $$$$$$26,
-    "elux-components-app/sections/blog/BlogListingBreadcrumb.tsx": $$$$$$27,
-    "elux-components-app/sections/blog/BlogpostBanner.tsx": $$$$$$30,
-    "elux-components-app/sections/blog/BlogpostBreadcrumb.tsx": $$$$$$31,
-    "elux-components-app/sections/blog/BlogpostComments.tsx": $$$$$$32,
-    "elux-components-app/sections/blog/BlogpostContent.tsx": $$$$$$33,
-    "elux-components-app/sections/blog/BlogSearch.tsx": $$$$$$28,
-    "elux-components-app/sections/blog/BlogShelf.tsx": $$$$$$29,
+    "elux-components-app/sections/blog/BlogListing.tsx": $$$$$$27,
+    "elux-components-app/sections/blog/BlogListingBreadcrumb.tsx": $$$$$$28,
+    "elux-components-app/sections/blog/BlogpostBanner.tsx": $$$$$$31,
+    "elux-components-app/sections/blog/BlogpostBreadcrumb.tsx": $$$$$$32,
+    "elux-components-app/sections/blog/BlogpostComments.tsx": $$$$$$33,
+    "elux-components-app/sections/blog/BlogpostContent.tsx": $$$$$$34,
+    "elux-components-app/sections/blog/BlogSearch.tsx": $$$$$$29,
+    "elux-components-app/sections/blog/BlogShelf.tsx": $$$$$$30,
     "elux-components-app/sections/Component.tsx": $$$$$$0,
-    "elux-components-app/sections/container/Container.tsx": $$$$$$34,
+    "elux-components-app/sections/container/Container.tsx": $$$$$$35,
     "elux-components-app/sections/Content/Banner.tsx": $$$$$$1,
     "elux-components-app/sections/Content/Breadcrumb.tsx": $$$$$$2,
     "elux-components-app/sections/Content/Cards.tsx": $$$$$$3,
     "elux-components-app/sections/Content/CategoryCards.tsx": $$$$$$4,
     "elux-components-app/sections/Content/HelpCards.tsx": $$$$$$5,
     "elux-components-app/sections/Content/ImageCards.tsx": $$$$$$6,
-    "elux-components-app/sections/Content/TextCards.tsx": $$$$$$7,
-    "elux-components-app/sections/Content/Toast.tsx": $$$$$$8,
-    "elux-components-app/sections/Footer/Footer.tsx": $$$$$$9,
-    "elux-components-app/sections/Header/Header.tsx": $$$$$$10,
-    "elux-components-app/sections/Images/BannersMatcher.tsx": $$$$$$11,
-    "elux-components-app/sections/Images/Carousel.tsx": $$$$$$12,
-    "elux-components-app/sections/Institutional/GuidesCategories.tsx": $$$$$$13,
-    "elux-components-app/sections/Institutional/GuidesDocuments.tsx": $$$$$$14,
-    "elux-components-app/sections/Institutional/GuidesProducts.tsx": $$$$$$15,
-    "elux-components-app/sections/Institutional/Policy.tsx": $$$$$$16,
-    "elux-components-app/sections/Institutional/Support.tsx": $$$$$$17,
-    "elux-components-app/sections/Institutional/WhereToBuy.tsx": $$$$$$18,
-    "elux-components-app/sections/PageView.tsx": $$$$$$19,
+    "elux-components-app/sections/Content/RawText.tsx": $$$$$$7,
+    "elux-components-app/sections/Content/TextCards.tsx": $$$$$$8,
+    "elux-components-app/sections/Content/Toast.tsx": $$$$$$9,
+    "elux-components-app/sections/Footer/Footer.tsx": $$$$$$10,
+    "elux-components-app/sections/Header/Header.tsx": $$$$$$11,
+    "elux-components-app/sections/Images/BannersMatcher.tsx": $$$$$$12,
+    "elux-components-app/sections/Images/Carousel.tsx": $$$$$$13,
+    "elux-components-app/sections/Institutional/GuidesCategories.tsx": $$$$$$14,
+    "elux-components-app/sections/Institutional/GuidesDocuments.tsx": $$$$$$15,
+    "elux-components-app/sections/Institutional/GuidesProducts.tsx": $$$$$$16,
+    "elux-components-app/sections/Institutional/Policy.tsx": $$$$$$17,
+    "elux-components-app/sections/Institutional/Support.tsx": $$$$$$18,
+    "elux-components-app/sections/Institutional/WhereToBuy.tsx": $$$$$$19,
+    "elux-components-app/sections/PageView.tsx": $$$$$$20,
     "elux-components-app/sections/Product/ProductDetails/ProductBannerMatcher.tsx":
-      $$$$$$20,
-    "elux-components-app/sections/Product/ProductDetails/ProductBreadcrumb.tsx":
       $$$$$$21,
-    "elux-components-app/sections/Product/ProductDetails/ProductPage.tsx":
+    "elux-components-app/sections/Product/ProductDetails/ProductBreadcrumb.tsx":
       $$$$$$22,
-    "elux-components-app/sections/Product/ProductListing/ProductListingPage.tsx":
+    "elux-components-app/sections/Product/ProductDetails/ProductPage.tsx":
       $$$$$$23,
-    "elux-components-app/sections/Product/ProductShelf/ProductShelf.tsx":
+    "elux-components-app/sections/Product/ProductListing/ProductListingPage.tsx":
       $$$$$$24,
-    "elux-components-app/sections/Social/ContactForm.tsx": $$$$$$25,
+    "elux-components-app/sections/Product/ProductShelf/ProductShelf.tsx":
+      $$$$$$25,
+    "elux-components-app/sections/Social/ContactForm.tsx": $$$$$$26,
   },
   "actions": {
     "elux-components-app/actions/categories/createCategory.ts": $$$$$$$$$0,
